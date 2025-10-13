aebace70ec1774035589f33dc9fd1c3b182a6cc2
Damian <josepwnz@gmail.com>
2025-10-14 01:37:56 +0200

    refactor: moved/removed

--- src/main/java/com/damian/xBank/modules/customer/exception/CustomerException.java
+++
@@ -1,9 +0,0 @@
-package com.damian.xBank.modules.customer.exception;
-
-import com.damian.xBank.shared.exception.ApplicationException;
-
-public class CustomerException extends ApplicationException {
-    public CustomerException(String message) {
-        super(message);
-    }
-}
