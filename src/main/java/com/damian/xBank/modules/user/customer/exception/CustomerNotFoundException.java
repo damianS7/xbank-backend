5daac4206d79a8cddaa022dc1c366dd07ba8e678
Damian <josepwnz@gmail.com>
2025-10-16 00:55:02 +0200

    added CustomerExceptionHandler y new exceptions

--- src/main/java/com/damian/xBank/modules/user/customer/exception/CustomerNotFoundException.java
+++ src/main/java/com/damian/xBank/modules/user/customer/exception/CustomerNotFoundException.java
@@ -1,7 +1,7 @@
 package com.damian.xBank.modules.user.customer.exception;
 
 public class CustomerNotFoundException extends CustomerException {
-    public CustomerNotFoundException(String message) {
-        super(message);
+    public CustomerNotFoundException(String message, Long customerId) {
+        super(message, customerId);
     }
 }
