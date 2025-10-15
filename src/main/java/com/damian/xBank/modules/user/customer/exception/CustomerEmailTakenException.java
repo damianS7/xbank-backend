cb28375d2a7752834fddf262b6f51849e4d588e7
Damian <josepwnz@gmail.com>
2025-10-16 00:55:15 +0200

    removed unused exception

--- src/main/java/com/damian/xBank/modules/user/customer/exception/CustomerEmailTakenException.java
+++
@@ -1,7 +0,0 @@
-package com.damian.xBank.modules.user.customer.exception;
-
-public class CustomerEmailTakenException extends CustomerException {
-    public CustomerEmailTakenException(String message) {
-        super(message);
-    }
-}
