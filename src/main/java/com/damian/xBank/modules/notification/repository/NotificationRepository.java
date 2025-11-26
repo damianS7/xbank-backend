f0d1ad92deff48f32c1974124ec7a7ae4dbc5eb6
Damian <josepwnz@gmail.com>
2025-11-27 00:20:09 +0100

    added method to receive all the notification ids to be deleted

--- src/main/java/com/damian/xBank/modules/notification/repository/NotificationRepository.java
+++ src/main/java/com/damian/xBank/modules/notification/repository/NotificationRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findAllByUserId(Long userId, Pageable pageable);
 
+
     void deleteAllByUser_Id(Long userId);
+
+    void deleteAllByIdInAndUser_Id(List<Long> notificationIds, Long userId);
 }
 
