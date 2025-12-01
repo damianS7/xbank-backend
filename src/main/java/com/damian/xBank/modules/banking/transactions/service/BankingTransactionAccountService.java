691432f42972d0ef1643386f50d35c938c868f0e
Damian <josepwnz@gmail.com>
2025-12-02 00:09:13 +0100

    deleted BankingTransactionService: - use of BankingTransactionAccount and Card services instead BankingTransactionService

--- src/main/java/com/damian/xBank/modules/banking/transactions/service/BankingTransactionAccountService.java
+++ src/main/java/com/damian/xBank/modules/banking/transactions/service/BankingTransactionAccountService.java
@@ -1,209 +1,143 @@
 package com.damian.xBank.modules.banking.transactions.service;
 
-import com.damian.xBank.modules.banking.account.BankingAccountAuthorizationHelper;
-import com.damian.xBank.modules.banking.account.exception.BankingAccountAuthorizationException;
-import com.damian.xBank.modules.banking.account.exception.BankingAccountNotFoundException;
 import com.damian.xBank.modules.banking.account.repository.BankingAccountRepository;
-import com.damian.xBank.modules.banking.transactions.dto.request.BankingAccountTransactionRequest;
-import com.damian.xBank.modules.banking.transactions.enums.BankingTransactionStatus;
+import com.damian.xBank.modules.banking.transactions.dto.request.BankingTransactionUpdateStatusRequest;
 import com.damian.xBank.modules.banking.transactions.enums.BankingTransactionType;
-import com.damian.xBank.modules.banking.transactions.exception.BankingTransactionException;
+import com.damian.xBank.modules.banking.transactions.exception.BankingTransactionAuthorizationException;
+import com.damian.xBank.modules.banking.transactions.exception.BankingTransactionNotFoundException;
+import com.damian.xBank.modules.banking.transactions.repository.BankingTransactionRepository;
+import com.damian.xBank.modules.user.account.account.enums.UserAccountRole;
 import com.damian.xBank.shared.domain.BankingAccount;
 import com.damian.xBank.shared.domain.BankingTransaction;
 import com.damian.xBank.shared.domain.Customer;
 import com.damian.xBank.shared.exception.Exceptions;
 import com.damian.xBank.shared.utils.AuthHelper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Service
 public class BankingTransactionAccountService {
     private final BankingAccountRepository bankingAccountRepository;
-    private final BankingTransactionService bankingTransactionService;
+    private final BankingTransactionRepository bankingTransactionRepository;
 
     public BankingTransactionAccountService(
             BankingAccountRepository bankingAccountRepository,
-            BankingTransactionService bankingTransactionService
+            BankingTransactionRepository bankingTransactionRepository
     ) {
         this.bankingAccountRepository = bankingAccountRepository;
-        this.bankingTransactionService = bankingTransactionService;
+        this.bankingTransactionRepository = bankingTransactionRepository;
     }
 
-    // handle request BankingTransactionType and determine what to do.
-    public BankingTransaction processTransactionRequest(
-            Long fromAccountId,
-            BankingAccountTransactionRequest request
-    ) {
-        // BankingAccount to operate
-        BankingAccount bankingAccount = bankingAccountRepository.findById(fromAccountId).orElseThrow(
-                () -> new BankingAccountNotFoundException(
-                        fromAccountId
-                )
-        );
-
-        return switch (request.transactionType()) {
-            case TRANSFER_TO -> this.transferTo(
-                    bankingAccount,
-                    request.toBankingAccountNumber(),
-                    request.password(),
-                    request.amount(),
-                    request.description()
-            );
-            case DEPOSIT -> this.deposit(bankingAccount, request.password(), request.amount());
-            default -> throw new BankingTransactionException(
-                    Exceptions.BANKING.TRANSACTION.INVALID_TYPE, -1L
-            );
-        };
-    }
-
-    // validates all security checks before transfer
-    public void validateTransferOrElseThrow(
-            BankingAccount fromBankingAccount,
-            BankingAccount toBankingAccount,
-            BigDecimal amount
-    ) {
-        // check bankingAccount and toBankingAccount are not the same
-        if (fromBankingAccount.getId().equals(toBankingAccount.getId())) {
-            throw new BankingAccountAuthorizationException(
-                    Exceptions.BANKING.ACCOUNT.SAME_DESTINATION,
-                    toBankingAccount.getId(),
-                    fromBankingAccount.getOwner().getId()
-            );
-        }
-
-        // check currency are the same
-        this.checkCurrency(fromBankingAccount, toBankingAccount);
-
-        // check the funds from the sender account
-        this.checkFunds(fromBankingAccount, amount);
-
-        // check the account status and see if can be used to operate
-        BankingAccountAuthorizationHelper
-                .authorize(null, fromBankingAccount)
-                .checkAccountStatus();
-
-        BankingAccountAuthorizationHelper
-                .authorize(null, toBankingAccount)
-                .checkAccountStatus();
-    }
-
-
-    // check the funds from the account
-    private void checkFunds(BankingAccount bankingAccount, BigDecimal amount) {
-        if (!bankingAccount.hasEnoughFunds(amount)) {
-            throw new BankingAccountAuthorizationException(
-                    Exceptions.BANKING.ACCOUNT.INSUFFICIENT_FUNDS, bankingAccount.getId(), 0L // TODO
-            );
-        }
-    }
-
-    // check currency are the same
-    private void checkCurrency(BankingAccount fromBankingAccount, BankingAccount toBankingAccount) {
-        if (!fromBankingAccount.getAccountCurrency()
-                               .equals(toBankingAccount.getAccountCurrency())
-        ) {
-            throw new BankingAccountAuthorizationException(
-                    Exceptions.BANKING.TRANSACTION.DIFFERENT_CURRENCY,
-                    toBankingAccount.getId(),
-                    fromBankingAccount.getOwner().getId()
-            );
-        }
-    }
-
-    // validates account status and does the transaction
-    public BankingTransaction transferTo(
-            BankingAccount fromBankingAccount,
-            String toBankingAccountNumber,
-            String password,
+    public BankingTransaction createTransaction(
+            BankingAccount bankingAccount,
+            BankingTransactionType transactionType,
             BigDecimal amount,
             String description
     ) {
-        // Banking account to receive funds
-        final BankingAccount toBankingAccount = bankingAccountRepository
-                .findByAccountNumber(toBankingAccountNumber)
+        return BankingTransaction.create()
+                                 .setAssociatedBankingAccount(bankingAccount)
+                                 .setTransactionType(transactionType)
+                                 .setLastBalance(bankingAccount.getBalance())
+                                 .setAmount(amount)
+                                 .setDescription(description);
+    }
+
+    public Page<BankingTransaction> getTransactions(Long bankingAccountId, Pageable pageable) {
+        return bankingTransactionRepository.findByBankingAccountId(bankingAccountId, pageable);
+    }
+
+    // ...
+
+    public BankingTransaction generateTransaction(
+            BankingAccount bankingAccount,
+            BankingTransactionType transactionType,
+            BigDecimal amount,
+            String description
+    ) {
+        // createTransaction
+        BankingTransaction transaction = this.createTransaction(bankingAccount, transactionType, amount, description);
+
+        // persistTransaction
+        this.persistTransaction(transaction);
+
+        return transaction;
+    }
+
+    /**
+     * Stores a banking account transaction by adding it to the owner's account and persisting the account.
+     *
+     * @param transaction the banking account transaction to store
+     * @return the stored banking account transaction
+     */
+    // TODO rename method to commitTransaction?
+    public BankingTransaction persistTransaction(BankingTransaction transaction) {
+        final BankingAccount bankingAccount = transaction.getAssociatedBankingAccount();
+        transaction.setCreatedAt(Instant.now());
+        transaction.setUpdatedAt(Instant.now());
+
+        // Add the transaction to the owners account
+        bankingAccount.addAccountTransaction(transaction);
+
+        // Persist the owner's account with the new transaction
+        // Return the stored transaction
+        return bankingTransactionRepository.save(transaction);
+    }
+
+    public BankingTransaction getBankingTransaction(Long transactionId) {
+        // Customer logged
+        final Customer currentCustomer = AuthHelper.getCurrentCustomer();
+
+        BankingTransaction transaction = bankingTransactionRepository.findById(transactionId).orElseThrow(
+                () -> new BankingTransactionNotFoundException(transactionId)
+        );
+
+        if (!transaction.getAssociatedBankingAccount().getOwner().getId().equals(currentCustomer.getId())
+            && !currentCustomer.getAccount().getRole().equals(UserAccountRole.ADMIN)) {
+            throw new BankingTransactionAuthorizationException(
+                    Exceptions.BANKING.TRANSACTION.ACCESS_FORBIDDEN, transactionId
+            );
+        }
+
+        return transaction;
+    }
+
+
+    // it changes the status of the transaction
+    public BankingTransaction updateTransactionStatus(
+            Long bankingTransactionId,
+            BankingTransactionUpdateStatusRequest request
+    ) {
+        // Customer logged
+        final Customer currentCustomer = AuthHelper.getCurrentCustomer();
+
+        // if the logged customer is not admin
+        if (!currentCustomer.getAccount().getRole().equals(UserAccountRole.ADMIN)) {
+            // banking transaction does not belong to this customer
+            throw new BankingTransactionAuthorizationException(
+                    Exceptions.BANKING.TRANSACTION.ACCESS_FORBIDDEN, bankingTransactionId
+            );
+        }
+
+        // transaction to update
+        final BankingTransaction bankingTransaction = bankingTransactionRepository
+                .findById(bankingTransactionId)
                 .orElseThrow(
-                        () -> new BankingAccountNotFoundException(
-                                Exceptions.BANKING.ACCOUNT.NOT_FOUND, toBankingAccountNumber
+                        () -> new BankingTransactionNotFoundException(
+                                bankingTransactionId
                         )
                 );
 
-        final Customer customer = AuthHelper.getCurrentCustomer();
+        // we mark the account as closed
+        bankingTransaction.setStatus(request.transactionStatus());
 
-        // check if the account belongs to this customer.
-        BankingAccountAuthorizationHelper
-                .authorize(customer, fromBankingAccount)
-                .checkOwner()
-                .checkAccountStatus();
+        // we change the updateAt timestamp field
+        bankingTransaction.setUpdatedAt(Instant.now());
 
-        AuthHelper.validatePassword(customer, password);
-
-        // check transfer is valid
-        this.validateTransferOrElseThrow(fromBankingAccount, toBankingAccount, amount);
-
-        return this.transferTo(fromBankingAccount, toBankingAccount, amount, description);
-    }
-
-    public BankingTransaction transferTo(
-            BankingAccount fromBankingAccount,
-            BankingAccount toBankingAccount,
-            BigDecimal amount,
-            String description
-    ) {
-        BankingTransaction fromTransaction = this.bankingTransactionService.createTransaction(
-                fromBankingAccount,
-                BankingTransactionType.TRANSFER_TO,
-                amount,
-                description
-        );
-
-        fromBankingAccount.subtractAmount(amount);
-        fromTransaction.setStatus(BankingTransactionStatus.COMPLETED);
-        this.bankingTransactionService.persistTransaction(fromTransaction);
-
-        // create transfer transaction for the receiver of the funds
-        BankingTransaction toTransaction = this.bankingTransactionService.createTransaction(
-                toBankingAccount,
-                BankingTransactionType.TRANSFER_FROM,
-                amount,
-                "Transfer from " + fromBankingAccount.getOwner().getFullName()
-        );
-
-        toBankingAccount.deposit(amount);
-        toTransaction.setStatus(BankingTransactionStatus.COMPLETED);
-        this.bankingTransactionService.persistTransaction(toTransaction);
-
-        return fromTransaction;
-    }
-
-    // validates account status and does the transaction
-    public BankingTransaction deposit(
-            BankingAccount account,
-            String password,
-            BigDecimal amount
-    ) {
-        BankingTransaction transaction = this.bankingTransactionService.createTransaction(
-                account,
-                BankingTransactionType.DEPOSIT,
-                amount,
-                "DEPOSIT"
-        );
-
-        final Customer customer = AuthHelper.getCurrentCustomer();
-
-        // check if the account belongs to this customer.
-        BankingAccountAuthorizationHelper
-                .authorize(customer, account)
-                .checkOwner()
-                .checkAccountStatus();
-
-        // if the transaction is created, deduce the amount from balance
-        account.deposit(amount);
-
-        // transaction is completed
-        transaction.setStatus(BankingTransactionStatus.COMPLETED);
-
-        // save the transaction
-        return bankingTransactionService.persistTransaction(transaction);
+        // save the data and return BankingAccount
+        return bankingTransactionRepository.save(bankingTransaction);
     }
 }
--- src/main/java/com/damian/xBank/modules/banking/transactions/service/BankingTransactionCardService.java
+++ src/main/java/com/damian/xBank/modules/banking/transactions/service/BankingTransactionCardService.java
@@ -1,137 +1,63 @@
 package com.damian.xBank.modules.banking.transactions.service;
 
-import com.damian.xBank.modules.banking.card.BankingCardAuthorizationHelper;
-import com.damian.xBank.modules.banking.card.exception.BankingCardAuthorizationException;
-import com.damian.xBank.modules.banking.card.exception.BankingCardNotFoundException;
 import com.damian.xBank.modules.banking.card.repository.BankingCardRepository;
-import com.damian.xBank.modules.banking.transactions.dto.request.BankingCardTransactionRequest;
-import com.damian.xBank.modules.banking.transactions.enums.BankingTransactionStatus;
 import com.damian.xBank.modules.banking.transactions.enums.BankingTransactionType;
-import com.damian.xBank.modules.banking.transactions.exception.BankingTransactionException;
+import com.damian.xBank.modules.banking.transactions.repository.BankingTransactionRepository;
 import com.damian.xBank.shared.domain.BankingCard;
 import com.damian.xBank.shared.domain.BankingTransaction;
-import com.damian.xBank.shared.domain.Customer;
-import com.damian.xBank.shared.exception.Exceptions;
-import com.damian.xBank.shared.utils.AuthHelper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 public class BankingTransactionCardService {
-
     private final BankingCardRepository bankingCardRepository;
-    private final BankingTransactionService bankingTransactionService;
+    private final BankingTransactionRepository bankingTransactionRepository;
+    private final BankingTransactionAccountService bankingTransactionAccountService;
 
     public BankingTransactionCardService(
             BankingCardRepository bankingCardRepository,
-            BankingTransactionService bankingTransactionService
+            BankingTransactionRepository bankingTransactionRepository,
+            BankingTransactionAccountService bankingTransactionAccountService
     ) {
         this.bankingCardRepository = bankingCardRepository;
-        this.bankingTransactionService = bankingTransactionService;
+        this.bankingTransactionRepository = bankingTransactionRepository;
+        this.bankingTransactionAccountService = bankingTransactionAccountService;
     }
 
-    // handle request BankingTransactionType and determine what to do.
-    public BankingTransaction processTransactionRequest(
-            Long cardId,
-            BankingCardTransactionRequest request
-    ) {
-        // BankingCard to operate
-        BankingCard bankingCard = bankingCardRepository.findById(cardId).orElseThrow(
-                () -> new BankingCardNotFoundException(
-                        Exceptions.BANKING.CARD.NOT_FOUND, cardId
-                )
-        );
-
-        return switch (request.transactionType()) {
-            case CARD_CHARGE -> this.spend(bankingCard, request.cardPin(), request.amount(), request.description());
-            case WITHDRAWAL -> this.withdrawal(bankingCard, request.cardPin(), request.amount());
-            default -> throw new BankingTransactionException(
-                    Exceptions.BANKING.TRANSACTION.INVALID_TYPE, -1L
-            );
-        };
+    public Page<BankingTransaction> getTransactions(Long bankingCardId, Pageable pageable) {
+        return bankingTransactionRepository.findByBankingCardId(bankingCardId, pageable);
     }
 
-    private void canCarryOperationOrElseThrow(
-            BankingCard card,
-            Customer customerLogged,
-            String cardPIN,
-            BigDecimal amount
-    ) {
-        BankingCardAuthorizationHelper
-                .authorize(customerLogged, card)
-                // check customer authorization
-                .checkOwner()
-                // check if card is not disabled or locked
-                .checkStatus()
-                .checkPIN(cardPIN);
-
-        // check balance
-        this.checkFunds(card, amount);
-    }
-
-    public void checkFunds(BankingCard card, BigDecimal amount) {
-        if (!card.hasEnoughFundsToSpend(amount)) {
-            throw new BankingCardAuthorizationException(
-                    Exceptions.BANKING.CARD.INSUFFICIENT_FUNDS, card.getId(), 0L // TODO
-            );
-        }
-    }
-
-    // validates card status and does the transaction
-    public BankingTransaction spend(
-            BankingCard card,
-            String cardPIN,
+    public BankingTransaction createTransaction(
+            BankingCard bankingCard,
+            BankingTransactionType transactionType,
             BigDecimal amount,
             String description
     ) {
-        BankingTransaction transaction = this.bankingTransactionService.createTransaction(
-                card,
-                BankingTransactionType.CARD_CHARGE,
+        return bankingTransactionAccountService.createTransaction(
+                bankingCard.getAssociatedBankingAccount(),
+                transactionType,
                 amount,
                 description
         );
-        final Customer customerLogged = AuthHelper.getCurrentCustomer();
-
-        // run validations and throw if any throw exception
-        this.canCarryOperationOrElseThrow(card, customerLogged, cardPIN, amount);
-
-        // if the transaction is created, deduce the amount from balance
-        card.chargeAmount(amount);
-
-        // transaction is completed
-        transaction.setStatus(BankingTransactionStatus.COMPLETED);
-
-        // save the transaction
-        return bankingTransactionService.persistTransaction(transaction);
     }
 
-    // withdraws money
-    public BankingTransaction withdrawal(
-            BankingCard card,
-            String cardPIN,
-            BigDecimal amount
+    public BankingTransaction generateTransaction(
+            BankingCard bankingCard,
+            BankingTransactionType transactionType,
+            BigDecimal amount,
+            String description
     ) {
-        BankingTransaction transaction = this.bankingTransactionService.createTransaction(
-                card,
-                BankingTransactionType.WITHDRAWAL,
+        BankingTransaction transaction = bankingTransactionAccountService.createTransaction(
+                bankingCard.getAssociatedBankingAccount(),
+                transactionType,
                 amount,
-                "ATM withdrawal."
+                description
         );
 
-        final Customer customerLogged = AuthHelper.getCurrentCustomer();
-        this.canCarryOperationOrElseThrow(card, customerLogged, cardPIN, amount);
-
-        // check balance
-        this.checkFunds(card, amount);
-
-        // if the transaction is created, deduce the amount from balance
-        card.chargeAmount(amount);
-
-        // transaction is completed
-        transaction.setStatus(BankingTransactionStatus.COMPLETED);
-
-        // save the transaction
-        return bankingTransactionService.persistTransaction(transaction);
+        return bankingTransactionAccountService.persistTransaction(transaction);
     }
 }
