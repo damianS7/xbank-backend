c92d2a0ffcce847ccc726b7646d20f296ed9012a
Damian <josepwnz@gmail.com>
2025-12-02 00:06:02 +0100

    Removed old BankingTransactionRequest. Use specific request: BankingCardSpendRequest and BankingCardWithdrawRequest

---
+++ src/main/java/com/damian/xBank/modules/banking/card/dto/request/BankingCardSpendRequest.java
@@ -0,0 +1,22 @@
+package com.damian.xBank.modules.banking.card.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record BankingCardSpendRequest(
+        @Positive
+        BigDecimal amount,
+
+        @NotNull(
+                message = "card pin must not be null"
+        )
+        String cardPIN,
+
+        @NotNull(
+                message = "description pin must not be null"
+        )
+        String description
+) {
+}
---
+++ src/main/java/com/damian/xBank/modules/banking/card/dto/request/BankingCardWithdrawRequest.java
@@ -0,0 +1,17 @@
+package com.damian.xBank.modules.banking.card.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record BankingCardWithdrawRequest(
+        @Positive
+        BigDecimal amount,
+
+        @NotNull(
+                message = "card pin must not be null"
+        )
+        String cardPIN
+) {
+}
