3e4d3c084137e8f741597e95cead813c9f545875
Damian <josepwnz@gmail.com>
2025-12-04 22:37:15 +0100

    refactor: removed unused code. fixed test.

--- src/main/java/com/damian/xBank/modules/banking/account/application/service/admin/AdminBankingAccountOperationService.java
+++ src/main/java/com/damian/xBank/modules/banking/account/application/service/admin/AdminBankingAccountOperationService.java
@@ -1,27 +1,16 @@
 package com.damian.xBank.modules.banking.account.application.service.admin;
 
 import com.damian.xBank.modules.banking.account.application.dto.request.BankingAccountDepositRequest;
-import com.damian.xBank.modules.banking.account.application.dto.request.BankingAccountTransferRequest;
 import com.damian.xBank.modules.banking.account.application.guard.BankingAccountGuard;
 import com.damian.xBank.modules.banking.account.domain.entity.BankingAccount;
 import com.damian.xBank.modules.banking.account.domain.exception.BankingAccountNotFoundException;
 import com.damian.xBank.modules.banking.account.infra.repository.BankingAccountRepository;
-import com.damian.xBank.modules.banking.transaction.application.dto.mapper.BankingTransactionDtoMapper;
 import com.damian.xBank.modules.banking.transaction.application.service.BankingTransactionAccountService;
 import com.damian.xBank.modules.banking.transaction.domain.entity.BankingTransaction;
 import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionStatus;
 import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionType;
 import com.damian.xBank.modules.notification.application.service.NotificationService;
-import com.damian.xBank.modules.notification.domain.enums.NotificationType;
-import com.damian.xBank.modules.notification.domain.event.NotificationEvent;
-import com.damian.xBank.modules.user.customer.domain.entity.Customer;
-import com.damian.xBank.shared.exception.Exceptions;
-import com.damian.xBank.shared.utils.AuthHelper;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.Map;
 
 @Service
 public class AdminBankingAccountOperationService {
@@ -50,11 +39,6 @@ public class AdminBankingAccountOperationService {
             Long bankingAccountId,
             BankingAccountDepositRequest request
     ) {
-        // Customer logged
-        final Customer currentCustomer = AuthHelper.getCurrentCustomer();
-
-        // TODO check currentCustomer is an employee or admin
-
         // The account to deposit into
         final BankingAccount bankingAccount = bankingAccountRepository
                 .findById(bankingAccountId).orElseThrow(
@@ -82,102 +66,5 @@ public class AdminBankingAccountOperationService {
 
         // save the transaction
         return bankingTransactionAccountService.persistTransaction(transaction);
-    }
-
-    public BankingTransaction transfer(
-            Long fromBankingAccountId,
-            BankingAccountTransferRequest request
-    ) {
-
-        // Banking account to receive funds
-        final BankingAccount fromBankingAccount = bankingAccountRepository
-                .findById(fromBankingAccountId)
-                .orElseThrow(
-                        () -> new BankingAccountNotFoundException(
-                                Exceptions.BANKING.ACCOUNT.NOT_FOUND, fromBankingAccountId
-                        )
-                );
-
-        return this.transferTo(
-                fromBankingAccount,
-                request.toBankingAccountNumber(),
-                request.password(),
-                request.amount(),
-                request.description()
-        );
-    }
-
-    // validates account status and does the transaction
-    public BankingTransaction transferTo(
-            BankingAccount fromBankingAccount,
-            String toBankingAccountNumber,
-            String password,
-            BigDecimal amount,
-            String description
-    ) {
-        // Banking account to receive funds
-        final BankingAccount toBankingAccount = bankingAccountRepository
-                .findByAccountNumber(toBankingAccountNumber)
-                .orElseThrow(
-                        () -> new BankingAccountNotFoundException(
-                                Exceptions.BANKING.ACCOUNT.NOT_FOUND, toBankingAccountNumber
-                        )
-                );
-
-        final Customer customer = AuthHelper.getCurrentCustomer();
-
-        AuthHelper.validatePassword(customer, password);
-
-        // run validations and throw if any throw exception
-        BankingAccountGuard
-                .forAccount(fromBankingAccount)
-                .ownership(customer)
-                .transfer(toBankingAccount, amount);
-
-        return this.transferTo(fromBankingAccount, toBankingAccount, amount, description);
-    }
-
-    public BankingTransaction transferTo(
-            BankingAccount fromBankingAccount,
-            BankingAccount toBankingAccount,
-            BigDecimal amount,
-            String description
-    ) {
-        BankingTransaction fromTransaction = this.bankingTransactionAccountService.createTransaction(
-                fromBankingAccount,
-                BankingTransactionType.TRANSFER_TO,
-                amount,
-                description
-        );
-
-        fromBankingAccount.subtractBalance(amount);
-        fromTransaction.setStatus(BankingTransactionStatus.COMPLETED);
-        this.bankingTransactionAccountService.persistTransaction(fromTransaction);
-
-        // create transfer transaction for the receiver of the funds
-        BankingTransaction toTransaction = this.bankingTransactionAccountService.createTransaction(
-                toBankingAccount,
-                BankingTransactionType.TRANSFER_FROM,
-                amount,
-                "Transfer from " + fromBankingAccount.getOwner().getFullName()
-        );
-
-        toBankingAccount.addBalance(amount);
-        toTransaction.setStatus(BankingTransactionStatus.COMPLETED);
-        this.bankingTransactionAccountService.persistTransaction(toTransaction);
-
-        // TODO: Notify both parties about the transaction
-        notificationService.publishNotification(
-                new NotificationEvent(
-                        toBankingAccount.getOwner().getId(),
-                        NotificationType.TRANSACTION,
-                        Map.of(
-                                "transaction", BankingTransactionDtoMapper.toBankingTransactionDto(toTransaction)
-                        ),
-                        Instant.now().toString()
-                )
-        );
-
-        return fromTransaction;
     }
 }
---
+++ src/test/java/com/damian/xBank/modules/banking/account/admin/AdminBankingAccountOperationServiceTest.java
@@ -0,0 +1,95 @@
+package com.damian.xBank.modules.banking.account.admin;
+
+import com.damian.xBank.modules.banking.account.application.dto.request.BankingAccountDepositRequest;
+import com.damian.xBank.modules.banking.account.application.service.admin.AdminBankingAccountOperationService;
+import com.damian.xBank.modules.banking.account.domain.entity.BankingAccount;
+import com.damian.xBank.modules.banking.account.infra.repository.BankingAccountRepository;
+import com.damian.xBank.modules.banking.transaction.application.service.BankingTransactionAccountService;
+import com.damian.xBank.modules.banking.transaction.domain.entity.BankingTransaction;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionStatus;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionType;
+import com.damian.xBank.modules.user.account.account.domain.entity.UserAccount;
+import com.damian.xBank.modules.user.customer.domain.entity.Customer;
+import com.damian.xBank.shared.AbstractServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class AdminBankingAccountOperationServiceTest extends AbstractServiceTest {
+
+    @InjectMocks
+    private AdminBankingAccountOperationService adminBankingAccountOperationService;
+
+    @Mock
+    private BankingAccountRepository bankingAccountRepository;
+
+    @Mock
+    private BankingTransactionAccountService bankingTransactionAccountService;
+
+    @Test
+    @DisplayName("Should deposit")
+    void shouldDeposit() {
+        // given
+        UserAccount userAccount = UserAccount.create()
+                                             .setId(1L)
+                                             .setEmail("customer@demo.com")
+                                             .setPassword(passwordEncoder.encode(RAW_PASSWORD));
+
+        Customer customer = Customer.create()
+                                    .setId(1L)
+                                    .setAccount(userAccount);
+
+        //        setUpContext(customer);
+
+        BigDecimal givenDepositAmount = BigDecimal.valueOf(3000);
+
+        BankingAccount givenBankingAccount = new BankingAccount(customer);
+        givenBankingAccount.setId(2L);
+        givenBankingAccount.setBalance(BigDecimal.ZERO);
+        givenBankingAccount.setAccountNumber("US9900001111112233334444");
+
+        BankingTransaction givenBankingTransaction = new BankingTransaction(givenBankingAccount);
+        givenBankingTransaction.setTransactionType(BankingTransactionType.DEPOSIT);
+        givenBankingTransaction.setAmount(givenDepositAmount);
+
+        BankingAccountDepositRequest depositRequest = new BankingAccountDepositRequest(
+                givenBankingAccount.getAccountNumber(),
+                givenDepositAmount
+        );
+
+        when(bankingAccountRepository.findById(givenBankingAccount.getId())).thenReturn(Optional.of(
+                givenBankingAccount));
+
+        when(bankingTransactionAccountService.generateTransaction(
+                any(BankingAccount.class),
+                any(BankingTransactionType.class),
+                any(BigDecimal.class),
+                any(String.class)
+        )).thenReturn(givenBankingTransaction);
+
+        when(bankingTransactionAccountService.persistTransaction(
+                any(BankingTransaction.class)
+        )).thenReturn(givenBankingTransaction);
+
+        // then
+        BankingTransaction transaction = adminBankingAccountOperationService.deposit(
+                givenBankingAccount.getId(),
+                depositRequest
+        );
+
+        // then
+        assertThat(transaction).isNotNull();
+        assertThat(transaction.getTransactionType()).isEqualTo(givenBankingTransaction.getTransactionType());
+        assertThat(transaction.getStatus()).isEqualTo(BankingTransactionStatus.COMPLETED);
+        assertThat(givenBankingAccount.getBalance()).isEqualTo(givenDepositAmount);
+    }
+
+}
