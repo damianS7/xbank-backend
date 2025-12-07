5fdd82deb8588727773eadd5ce08c1566265d3f8
Damian <josepwnz@gmail.com>
2025-12-07 20:35:12 +0100

    refactor: code clean up, better semantic naming.

--- src/main/java/com/damian/xBank/modules/banking/card/application/guard/BankingCardGuard.java
+++ src/main/java/com/damian/xBank/modules/banking/card/application/guard/BankingCardGuard.java
@@ -1,16 +1,14 @@
 package com.damian.xBank.modules.banking.card.application.guard;
 
+import com.damian.xBank.modules.banking.card.domain.entity.BankingCard;
 import com.damian.xBank.modules.banking.card.domain.enums.BankingCardLockStatus;
 import com.damian.xBank.modules.banking.card.domain.enums.BankingCardStatus;
-import com.damian.xBank.modules.banking.card.domain.exception.BankingCardAuthorizationException;
-import com.damian.xBank.modules.banking.card.domain.exception.BankingCardException;
-import com.damian.xBank.modules.banking.card.domain.exception.BankingCardInsufficientFundsException;
-import com.damian.xBank.modules.banking.card.domain.exception.BankingCardOwnershipException;
-import com.damian.xBank.modules.banking.card.domain.entity.BankingCard;
+import com.damian.xBank.modules.banking.card.domain.exception.*;
 import com.damian.xBank.modules.user.customer.domain.entity.Customer;
 import com.damian.xBank.shared.exception.Exceptions;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class BankingCardGuard {
     private final BankingCard card;
@@ -24,16 +22,16 @@ public class BankingCardGuard {
     }
 
     /**
-     * Validate the ownership of the {@link #card}.
+     * Assert the ownership of the {@link #card} belongs to {@link Customer}.
      *
      * @param customer the customer to check ownership against
      * @return the current validator instance for chaining
      * @throws BankingCardOwnershipException if the card does not belong to the customer
      */
-    public BankingCardGuard ownership(Customer customer) {
+    public BankingCardGuard assertOwnership(Customer customer) {
 
         // compare card owner id with given customer id
-        if (!card.getOwner().getId().equals(customer.getId())) {
+        if (!Objects.equals(card.getOwner().getId(), customer.getId())) {
             throw new BankingCardOwnershipException(
                     Exceptions.BANKING.CARD.OWNERSHIP, card.getId(), customer.getId()
             );
@@ -43,27 +41,31 @@ public class BankingCardGuard {
     }
 
     /**
-     * Validate {@link #card} is not CLOSED or SUSPENDED.
+     * Assert {@link #card} is not DISABLED or LOCKED and can be used for any operation.
      *
      * @return the current validator instance for chaining
      * @throws BankingCardException if the card does not belong to the customer
      */
-    public BankingCardGuard active() {
+    public BankingCardGuard assertUsable() {
+
+        this.assertEnabled()
+            .assertUnlocked();
+
+        return this;
+    }
+
+    /**
+     * Assert {@link #card} is not DISABLED.
+     *
+     * @return the current validator instance for chaining
+     * @throws BankingCardDisabledException if the card is locked
+     */
+    public BankingCardGuard assertEnabled() {
 
         // check card status
-        final boolean isCardDisabled = card.getCardStatus().equals(BankingCardStatus.DISABLED);
-
-        if (isCardDisabled) {
-            throw new BankingCardAuthorizationException(
-                    Exceptions.BANKING.CARD.DISABLED, card.getId(), 0L // TODO
-            );
-        }
-
-        // check lock status
-        final boolean isCardLocked = card.getLockStatus().equals(BankingCardLockStatus.LOCKED);
-        if (isCardLocked) {
-            throw new BankingCardAuthorizationException(
-                    Exceptions.BANKING.CARD.LOCKED, card.getId(), 0L // TODO
+        if (Objects.equals(card.getCardStatus(), BankingCardStatus.DISABLED)) {
+            throw new BankingCardDisabledException(
+                    Exceptions.BANKING.CARD.DISABLED, card.getId()
             );
         }
 
@@ -71,17 +73,17 @@ public class BankingCardGuard {
     }
 
     /**
-     * Validate {@link #card} PIN.
+     * Assert {@link #card} is not LOCKED.
      *
      * @return the current validator instance for chaining
-     * @throws BankingCardException if the card does not belong to the customer
+     * @throws BankingCardLockedException if the card is locked
      */
-    public BankingCardGuard PIN(String PIN) {
+    public BankingCardGuard assertUnlocked() {
 
-        // check card pin
-        if (!card.getCardPin().equals(PIN)) {
-            throw new BankingCardAuthorizationException(
-                    Exceptions.BANKING.CARD.INVALID_PIN, card.getId(), 0L // TODO
+        // check lock status
+        if (Objects.equals(card.getLockStatus(), BankingCardLockStatus.LOCKED)) {
+            throw new BankingCardLockedException(
+                    Exceptions.BANKING.CARD.LOCKED, card.getId()
             );
         }
 
@@ -89,13 +91,31 @@ public class BankingCardGuard {
     }
 
     /**
-     * Validate if the {@link #card} has sufficient funds.
+     * Assert the {@link #card} PIN matches.
+     *
+     * @return the current validator instance for chaining
+     * @throws BankingCardInvalidPinException if the card PIN does not equals to the given PIN
+     */
+    public BankingCardGuard assertCorrectPin(String PIN) {
+
+        // check card pin
+        if (!Objects.equals(card.getCardPin(), PIN)) {
+            throw new BankingCardInvalidPinException(
+                    Exceptions.BANKING.CARD.INVALID_PIN, card.getId()
+            );
+        }
+
+        return this;
+    }
+
+    /**
+     * Assert the {@link #card} has sufficient funds.
      *
      * @param amount the amount to check
      * @return the current validator instance for chaining
      * @throws BankingCardOwnershipException if the card does not belong to the customer
      */
-    public BankingCardGuard sufficientFunds(BigDecimal amount) {
+    public BankingCardGuard assertSufficientFunds(BigDecimal amount) {
 
         // check if card has enough funds
         if (!card.hasEnoughFundsToSpend(amount)) {
@@ -108,15 +128,22 @@ public class BankingCardGuard {
         return this;
     }
 
-    public void canSpend(
+    /**
+     * Assert the {@link #card} has can spend funds.
+     * It checks card ownership, and, is usable, has funds, and has correct PIN.
+     *
+     * @param customer the customer to check ownership
+     * @param cardPIN  the card pin to check
+     * @param amount   the amount to check funds
+     */
+    public void assertCanSpend(
             Customer customer,
             String cardPIN,
             BigDecimal amount
     ) {
-        this.ownership(customer)
-            .active()
-            .sufficientFunds(amount)
-            .PIN(cardPIN);
-
+        this.assertOwnership(customer)
+            .assertUsable()
+            .assertSufficientFunds(amount)
+            .assertCorrectPin(cardPIN);
     }
 }
