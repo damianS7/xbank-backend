e892d8da3f78b6c25902903ec6cd8a40e53b2813
Damian <josepwnz@gmail.com>
2025-12-09 01:04:21 +0100

    added method

--- src/main/java/com/damian/xBank/modules/banking/account/application/service/BankingAccountService.java
+++ src/main/java/com/damian/xBank/modules/banking/account/application/service/BankingAccountService.java
@@ -6,8 +6,8 @@ import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountCurre
 import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountStatus;
 import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountType;
 import com.damian.xBank.modules.banking.account.infra.repository.BankingAccountRepository;
-import com.damian.xBank.modules.user.customer.domain.exception.CustomerNotFoundException;
 import com.damian.xBank.modules.user.customer.domain.entity.Customer;
+import com.damian.xBank.modules.user.customer.domain.exception.CustomerNotFoundException;
 import com.damian.xBank.modules.user.customer.infra.repository.CustomerRepository;
 import com.damian.xBank.shared.exception.Exceptions;
 import com.damian.xBank.shared.utils.AuthHelper;
--- src/main/java/com/damian/xBank/modules/user/customer/domain/entity/Customer.java
+++ src/main/java/com/damian/xBank/modules/user/customer/domain/entity/Customer.java
@@ -1,8 +1,8 @@
 package com.damian.xBank.modules.user.customer.domain.entity;
 
 import com.damian.xBank.modules.banking.account.domain.entity.BankingAccount;
-import com.damian.xBank.modules.user.account.account.domain.enums.UserAccountRole;
 import com.damian.xBank.modules.user.account.account.domain.entity.UserAccount;
+import com.damian.xBank.modules.user.account.account.domain.enums.UserAccountRole;
 import com.damian.xBank.modules.user.customer.domain.enums.CustomerGender;
 import jakarta.persistence.*;
 
@@ -21,7 +21,7 @@ public class Customer {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<BankingAccount> bankingAccounts;
 
-    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserAccount account;
 
@@ -91,6 +91,10 @@ public class Customer {
     public Customer setRole(UserAccountRole role) {
         this.account.setRole(role);
         return this;
+    }
+
+    public UserAccountRole getRole() {
+        return this.account.getRole();
     }
 
     public Long getId() {
