2e682b88cb34b46cfb95c24b472d12c7a8160ad3
Damian <josepwnz@gmail.com>
2025-12-13 01:06:40 +0100

    fixed test.

--- src/test/java/com/damian/xBank/modules/banking/transaction/infra/controller/BankingTransactionAccountControllerTest.java
+++ src/test/java/com/damian/xBank/modules/banking/transaction/infra/controller/BankingTransactionAccountControllerTest.java
@@ -1,68 +1,125 @@
 package com.damian.xBank.modules.banking.transaction.infra.controller;
 
-//import com.damian.xBank.modules.auth.http.AuthenticationRequest;
-//import com.damian.xBank.modules.auth.http.AuthenticationResponse;
-//import com.damian.xBank.modules.banking.account.*;
-//import com.damian.xBank.modules.banking.transactions.BankingTransaction;
-//import com.damian.xBank.modules.banking.transactions.BankingTransactionDTO;
-//import com.damian.xBank.modules.banking.transactions.BankingTransactionType;
-//import com.damian.xBank.modules.banking.transactions.http.BankingAccountTransactionRequest;
-//import com.damian.xBank.modules.customer.CustomerRole;
-//import com.damian.xBank.modules.user.customer.repository.CustomerRepository;
-//import com.damian.xBank.shared.domain.Customer;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.damian.xBank.modules.banking.account.domain.entity.BankingAccount;
+import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountCurrency;
+import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountStatus;
+import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountType;
+import com.damian.xBank.modules.banking.card.domain.entity.BankingCard;
+import com.damian.xBank.modules.banking.transaction.domain.entity.BankingTransaction;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionStatus;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionType;
+import com.damian.xBank.modules.user.account.account.domain.enums.UserAccountRole;
+import com.damian.xBank.modules.user.account.account.domain.enums.UserAccountStatus;
+import com.damian.xBank.modules.user.customer.domain.entity.Customer;
+import com.damian.xBank.modules.user.customer.domain.enums.CustomerGender;
+import com.damian.xBank.shared.AbstractControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 
-//@ActiveProfiles("test")
-//@SpringBootTest
-//@AutoConfigureMockMvc
-public class BankingTransactionAccountControllerTest {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-    //
-    //    @Test
-    //    @DisplayName("Should fetch transactions (pageable) for banking account")
-    //    void shouldFetchBankingAccountTransactions() throws Exception {
-    //        // given
-    //        loginWithCustomer(customerA);
-    //
-    //        BankingAccount bankingAccount = new BankingAccount(customerA);
-    //        bankingAccount.setAccountNumber("ES1234567890123456789012");
-    //        bankingAccount.setAccountType(BankingAccountType.SAVINGS);
-    //        bankingAccount.setAccountCurrency(BankingAccountCurrency.EUR);
-    //        bankingAccount.setAccountStatus(BankingAccountStatus.ACTIVE);
-    //        bankingAccount.setBalance(BigDecimal.valueOf(1000));
-    //        bankingAccountRepository.save(bankingAccount);
-    //
-    //        // when
-    //        // then
-    //        mockMvc
-    //                .perform(
-    //                        get("/api/v1/customers/me/banking/accounts/{id}/transactions", bankingAccount.getId())
-    //                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
-    //                .andDo(print())
-    //                .andExpect(status().is(200))
-    //                .andExpect(jsonPath("$.content").isArray())
-    //                .andExpect(jsonPath("$.content.length()").value(0)) // o el número que esperás
-    //                .andExpect(jsonPath("$.totalPages").value(0));
-    //    }
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class BankingTransactionAccountControllerTest extends AbstractControllerTest {
+    private Customer customer;
+    private BankingAccount customerBankingAccount;
+    private BankingCard customerBankingCard;
+
+    @BeforeEach
+    void setUp() {
+        customer = Customer.create()
+                           .setEmail("customer@demo.com")
+                           .setRole(UserAccountRole.CUSTOMER)
+                           .setPassword(passwordEncoder.encode(RAW_PASSWORD))
+                           .setFirstName("David")
+                           .setLastName("Brow")
+                           .setBirthdate(LocalDate.now())
+                           .setPhotoPath("avatar.jpg")
+                           .setPhone("123 123 123")
+                           .setPostalCode("01003")
+                           .setAddress("Fake ave")
+                           .setCountry("US")
+                           .setGender(CustomerGender.MALE);
+        customer.getAccount().setAccountStatus(UserAccountStatus.VERIFIED);
+        customerRepository.save(customer);
+
+        customerBankingAccount = BankingAccount
+                .create()
+                .setOwner(customer)
+                .setAccountCurrency(BankingAccountCurrency.EUR)
+                .setAccountType(BankingAccountType.SAVINGS)
+                .setAccountStatus(BankingAccountStatus.ACTIVE)
+                .setBalance(BigDecimal.valueOf(1000))
+                .setAccountNumber("US9900001111112233334444");
+
+
+        customerBankingCard = BankingCard
+                .create()
+                .setAssociatedBankingAccount(customerBankingAccount)
+                .setCardNumber("1234123412341234")
+                .setCardCvv("123")
+                .setCardPin("1234");
+
+        customerBankingAccount.addTransaction(
+                BankingTransaction
+                        .create()
+                        .setBankingAccount(customerBankingAccount)
+                        .setBankingCard(customerBankingCard)
+                        .setDescription("Amazon.com")
+                        .setBalanceBefore(BigDecimal.valueOf(1000))
+                        .setBalanceAfter(BigDecimal.valueOf(900))
+                        .setStatus(BankingTransactionStatus.PENDING)
+                        .setAmount(BigDecimal.valueOf(100))
+                        .setType(BankingTransactionType.CARD_CHARGE)
+        );
+
+        customerBankingAccount.addTransaction(
+                BankingTransaction
+                        .create()
+                        .setBankingAccount(customerBankingAccount)
+                        .setBankingCard(customerBankingCard)
+                        .setDescription("Netflix.com")
+                        .setBalanceBefore(BigDecimal.valueOf(900))
+                        .setBalanceAfter(BigDecimal.valueOf(870))
+                        .setStatus(BankingTransactionStatus.COMPLETED)
+                        .setAmount(BigDecimal.valueOf(30))
+                        .setType(BankingTransactionType.CARD_CHARGE)
+        );
+
+        customerBankingAccount.addTransaction(
+                BankingTransaction
+                        .create()
+                        .setBankingAccount(customerBankingAccount)
+                        .setBankingCard(customerBankingCard)
+                        .setDescription("HBO.com")
+                        .setBalanceBefore(BigDecimal.valueOf(870))
+                        .setBalanceAfter(BigDecimal.valueOf(850))
+                        .setStatus(BankingTransactionStatus.PENDING)
+                        .setAmount(BigDecimal.valueOf(20))
+                        .setType(BankingTransactionType.CARD_CHARGE)
+        );
+
+        customerBankingAccount.addBankingCard(customerBankingCard);
+        bankingAccountRepository.save(customerBankingAccount);
+    }
+
+    @Test
+    @DisplayName("Should get paged account transactions")
+    void shouldGetAccountTransactions() throws Exception {
+        // given
+        login(customer);
+
+        // when
+        // then
+        mockMvc.perform(get("/api/v1/banking/accounts/{id}/transactions", customerBankingAccount.getId())
+                       .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+               .andDo(print())
+               .andExpect(status().is(200));
+
+    }
 }
