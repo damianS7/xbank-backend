0914e4c98802e154854c49df4e93ba551b9010a6
Damian <josepwnz@gmail.com>
2025-12-24 22:53:04 +0100

    exceptions moved and renamed to BankingTransfer domain

--- src/main/java/com/damian/xBank/modules/banking/account/domain/exception/BankingAccountTransferException.java
+++
@@ -1,8 +0,0 @@
-package com.damian.xBank.modules.banking.account.domain.exception;
-
-public class BankingAccountTransferException extends BankingAccountException {
-    public BankingAccountTransferException(String message, Object resourceId) {
-        super(message, resourceId);
-    }
-
-}
