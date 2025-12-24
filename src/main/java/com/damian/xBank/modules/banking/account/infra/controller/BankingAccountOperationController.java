e954431ea063f9026e6d5d0138dfb3e8af324d9b
Damian <josepwnz@gmail.com>
2025-12-24 22:53:55 +0100

    logic moved to BankingTransfer

--- src/main/java/com/damian/xBank/modules/banking/account/infra/controller/BankingAccountOperationController.java
+++ src/main/java/com/damian/xBank/modules/banking/account/infra/controller/BankingAccountOperationController.java
@@ -1,16 +1,8 @@
 package com.damian.xBank.modules.banking.account.infra.controller;
 
-import com.damian.xBank.modules.banking.account.application.dto.request.BankingAccountTransferRequest;
 import com.damian.xBank.modules.banking.account.application.service.BankingAccountOperationService;
-import com.damian.xBank.modules.banking.transaction.application.dto.mapper.BankingTransactionDtoMapper;
-import com.damian.xBank.modules.banking.transaction.application.dto.response.BankingTransactionDto;
-import com.damian.xBank.modules.banking.transaction.domain.entity.BankingTransaction;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,20 +15,5 @@ public class BankingAccountOperationController {
         this.bankingAccountOperationService = bankingAccountOperationService;
     }
 
-    // endpoint for logged customer to transfer to another account
-    @PostMapping("/banking/accounts/{id}/transfer") // TODO remove
-    public ResponseEntity<?> transfer(
-            @PathVariable @NotNull @Positive
-            Long id,
-            @Validated @RequestBody
-            BankingAccountTransferRequest request
-    ) {
-        BankingTransaction transaction = bankingAccountOperationService.transfer(id, request);
-        BankingTransactionDto transactionDto = BankingTransactionDtoMapper.toBankingTransactionDto(transaction);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(transactionDto);
-    }
 }
 
