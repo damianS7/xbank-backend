92f1701616f973d26942e6c1a089416f8ce72a92
Damian <josepwnz@gmail.com>
2025-12-29 02:10:59 +0100

    added test for usecase

---
+++ src/test/java/com/damian/xBank/modules/banking/transfer/application/usecase/BankingTransferConfirmTest.java
@@ -0,0 +1,152 @@
+package com.damian.xBank.modules.banking.transfer.application.usecase;
+
+import com.damian.xBank.modules.banking.account.domain.entity.BankingAccount;
+import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountCurrency;
+import com.damian.xBank.modules.banking.account.domain.enums.BankingAccountType;
+import com.damian.xBank.modules.banking.account.infra.repository.BankingAccountRepository;
+import com.damian.xBank.modules.banking.transaction.application.service.BankingTransactionService;
+import com.damian.xBank.modules.banking.transaction.domain.entity.BankingTransaction;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionStatus;
+import com.damian.xBank.modules.banking.transaction.domain.enums.BankingTransactionType;
+import com.damian.xBank.modules.banking.transaction.infrastructure.repository.BankingTransactionRepository;
+import com.damian.xBank.modules.banking.transfer.application.dto.request.BankingTransferConfirmRequest;
+import com.damian.xBank.modules.banking.transfer.domain.model.BankingTransfer;
+import com.damian.xBank.modules.banking.transfer.domain.model.BankingTransferStatus;
+import com.damian.xBank.modules.banking.transfer.domain.service.BankingTransferService;
+import com.damian.xBank.modules.banking.transfer.infrastructure.repository.BankingTransferRepository;
+import com.damian.xBank.modules.notification.application.service.NotificationService;
+import com.damian.xBank.modules.notification.domain.event.NotificationEvent;
+import com.damian.xBank.modules.user.account.account.domain.entity.UserAccount;
+import com.damian.xBank.modules.user.customer.domain.entity.Customer;
+import com.damian.xBank.shared.AbstractServiceTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class BankingTransferConfirmTest extends AbstractServiceTest {
+
+    @InjectMocks
+    private BankingTransferConfirm bankingTransferConfirm;
+
+    @Mock
+    private BankingTransferService bankingTransferService;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Mock
+    private BankingAccountRepository bankingAccountRepository;
+
+    @Mock
+    private BankingTransferRepository bankingTransferRepository;
+
+    @Mock
+    private BankingTransactionRepository bankingTransactionRepository;
+
+    @Mock
+    private BankingTransactionService bankingTransactionService;
+
+    private Customer fromCustomer;
+    private Customer toCustomer;
+    private BankingAccount fromAccount;
+    private BankingAccount toAccount;
+
+    @BeforeEach
+    void setUp() {
+        fromCustomer = Customer.create(
+                UserAccount.create()
+                           .setId(1L)
+                           .setEmail("fromCustomer@demo.com")
+                           .setPassword(bCryptPasswordEncoder.encode(RAW_PASSWORD))
+        ).setId(1L);
+
+        fromAccount = BankingAccount
+                .create(fromCustomer)
+                .setId(1L)
+                .setBalance(BigDecimal.valueOf(1000))
+                .setCurrency(BankingAccountCurrency.EUR)
+                .setAccountType(BankingAccountType.SAVINGS)
+                .setAccountNumber("US9900001111112233334444");
+
+        toCustomer = Customer.create(
+                UserAccount.create()
+                           .setId(2L)
+                           .setEmail("toCustomer@demo.com")
+                           .setPassword(bCryptPasswordEncoder.encode(RAW_PASSWORD))
+        ).setId(2L);
+
+        toAccount = BankingAccount
+                .create(toCustomer)
+                .setId(2L)
+                .setBalance(BigDecimal.valueOf(1000))
+                .setCurrency(BankingAccountCurrency.EUR)
+                .setAccountType(BankingAccountType.SAVINGS)
+                .setAccountNumber("US1200001111112233335555");
+    }
+
+    @Test
+    @DisplayName("confirmTransfer a valid request should confirm a transfer")
+    void confirmTransfer_ValidRequest_ReturnsConfirmedTransfer() {
+        // given
+        setUpContext(fromCustomer);
+
+        BankingTransfer givenTransfer = BankingTransfer
+                .create(fromAccount, toAccount, BigDecimal.valueOf(100))
+                .setId(1L)
+                .setStatus(BankingTransferStatus.CONFIRMED)
+                .setDescription("a gift!");
+
+        BankingTransaction fromTransaction = BankingTransaction
+                .create(
+                        BankingTransactionType.TRANSFER_TO,
+                        fromAccount,
+                        givenTransfer.getAmount()
+                )
+                .setStatus(BankingTransactionStatus.PENDING)
+                .setDescription(givenTransfer.getDescription());
+
+        BankingTransaction toTransaction = BankingTransaction
+                .create(
+                        BankingTransactionType.TRANSFER_FROM,
+                        toAccount,
+                        givenTransfer.getAmount()
+                )
+                .setStatus(BankingTransactionStatus.PENDING)
+                .setDescription(givenTransfer.getDescription());
+
+        givenTransfer.addTransaction(fromTransaction);
+        givenTransfer.addTransaction(toTransaction);
+
+        BankingTransferConfirmRequest request = new BankingTransferConfirmRequest(
+                RAW_PASSWORD
+        );
+
+        // when
+        when(bankingTransferRepository.findById(anyLong())).thenReturn(Optional.of(givenTransfer));
+
+        when(bankingTransferService.confirmTransfer(anyLong(), any())).thenReturn(givenTransfer);
+
+        //        when(bankingAccountRepository.save(any(BankingAccount.class)))
+        //                .thenAnswer(i -> i.getArgument(0));
+
+        //        when(bankingTransferRepository.save(any(BankingTransfer.class)))
+        //                .thenAnswer(i -> i.getArgument(0));
+
+        doNothing().when(notificationService).publish(any(NotificationEvent.class));
+
+        // then
+        bankingTransferConfirm
+                .confirmTransfer(givenTransfer.getId(), request);
+
+        verify(bankingAccountRepository, times(2)).save(any(BankingAccount.class));
+        verify(bankingTransferRepository, times(1)).save(any(BankingTransfer.class));
+    }
+}
