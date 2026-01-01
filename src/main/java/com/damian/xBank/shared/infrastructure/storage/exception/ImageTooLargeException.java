d2b5193795047b197f3215dbdab10fcb28ed3671
Damian <josepwnz@gmail.com>
2026-01-01 23:38:44 +0100

    fixed test

--- src/main/java/com/damian/xBank/shared/infrastructure/storage/exception/ImageTooLargeException.java
+++ src/main/java/com/damian/xBank/shared/infrastructure/storage/exception/ImageTooLargeException.java
@@ -6,6 +6,6 @@ import com.damian.xBank.shared.exception.ErrorCodes;
 public class ImageTooLargeException extends ApplicationException {
 
     public ImageTooLargeException(String filename, Object size) {
-        super(ErrorCodes.STORAGE_UPLOAD_FILE_TOO_LARGE, filename, new Object[]{size});
+        super(ErrorCodes.STORAGE_IMAGE_UPLOAD_TOO_LARGE, filename, new Object[]{size});
     }
 }
