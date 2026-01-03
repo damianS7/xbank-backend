a8602a1dd325966ed7e87396d235d8489ed102cf
Damian <josepwnz@gmail.com>
2026-01-03 02:48:50 +0100

    renamed to profile

--- src/main/java/com/damian/xBank/modules/user/customer/domain/exception/CustomerUpdateAuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.damian.xBank.modules.user.customer.domain.exception;
-
-import com.damian.xBank.shared.exception.ErrorCodes;
-
-public class CustomerUpdateAuthorizationException extends CustomerException {
-    public CustomerUpdateAuthorizationException(Long customerId) {
-        super(ErrorCodes.CUSTOMER_UPDATE_FAILED, customerId);
-    }
-}
