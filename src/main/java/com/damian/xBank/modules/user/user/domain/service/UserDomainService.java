4fd15df2bf5effec779a39ac31ff2899faafd798
Damian <josepwnz@gmail.com>
2026-01-06 17:14:46 +0100

    code clean up

--- src/main/java/com/damian/xBank/modules/user/user/domain/service/UserDomainService.java
+++ src/main/java/com/damian/xBank/modules/user/user/domain/service/UserDomainService.java
@@ -3,8 +3,6 @@ package com.damian.xBank.modules.user.user.domain.service;
 import com.damian.xBank.modules.user.user.domain.exception.UserEmailTakenException;
 import com.damian.xBank.modules.user.user.domain.model.User;
 import com.damian.xBank.modules.user.user.domain.model.UserRole;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +10,12 @@ import java.time.Instant;
 
 @Service
 public class UserDomainService {
-    private static final Logger log = LoggerFactory.getLogger(UserDomainService.class);
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserDomainService(
-            BCryptPasswordEncoder bCryptPasswordEncoder
+            BCryptPasswordEncoder passwordEncoder
     ) {
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.bCryptPasswordEncoder = passwordEncoder;
     }
 
     /**
@@ -29,17 +26,13 @@ public class UserDomainService {
      * @return the user created
      * @throws UserEmailTakenException if another user has the email
      */
-    public User createUserAccount(String email, String password, UserRole role) {
+    public User createUser(String email, String password, UserRole role) {
 
         // we create the user and assign the data
-        User user = new User();
-        user.setEmail(email);
-        user.setRole(role);
-        user.setPassword(bCryptPasswordEncoder.encode(password));
-        user.setCreatedAt(Instant.now());
-
-        return user;
+        return User.create()
+                   .setEmail(email)
+                   .setRole(role)
+                   .setPassword(bCryptPasswordEncoder.encode(password))
+                   .setCreatedAt(Instant.now());
     }
-
-
 }
