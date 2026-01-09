048f64e8dcd5563d472f763fa73c1fd73b7346e0
Damian <josepwnz@gmail.com>
2026-01-09 23:30:56 +0100

    added new types and removed unused

--- src/main/java/com/damian/xBank/modules/notification/domain/model/NotificationType.java
+++ src/main/java/com/damian/xBank/modules/notification/domain/model/NotificationType.java
@@ -1,6 +1,12 @@
 package com.damian.xBank.modules.notification.domain.model;
 
 public enum NotificationType {
-    INFO,
-    TRANSACTION
+    TRANSFER_RECEIVED,
+    TRANSFER_SENT,
+    DEPOSIT_COMPLETED,
+    WITHDRAWAL_COMPLETED,
+    CARD_PAYMENT_DECLINED,
+    CARD_PAYMENT_PENDING,
+    CARD_EXPIRING_SOON,
+    ACCOUNT_CLOSED,
 }
