c19315cfc3e904179932c35fcd855449a48585f3
Damian <josepwnz@gmail.com>
2026-01-11 02:16:14 +0100

    renamed to payload

--- src/main/java/com/damian/xBank/modules/notification/application/dto/response/NotificationDto.java
+++ src/main/java/com/damian/xBank/modules/notification/application/dto/response/NotificationDto.java
@@ -2,13 +2,14 @@ package com.damian.xBank.modules.notification.application.dto.response;
 
 import com.damian.xBank.modules.notification.domain.model.NotificationType;
 
+import java.time.Instant;
 import java.util.Map;
 
 public record NotificationDto(
         Long id,
         NotificationType type,
-        Map<String, Object> metadata,
+        Map<String, Object> payload,
         String templateKey,
-        String createdAt
+        Instant createdAt
 ) {
 }
