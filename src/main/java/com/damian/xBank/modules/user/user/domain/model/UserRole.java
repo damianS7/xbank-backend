7519457872bb0a7b28f06317be129d49021824df
Damian <josepwnz@gmail.com>
2026-01-16 23:12:04 +0100

    removed role

--- src/main/java/com/damian/xBank/modules/user/user/domain/model/UserRole.java
+++ src/main/java/com/damian/xBank/modules/user/user/domain/model/UserRole.java
@@ -2,7 +2,6 @@ package com.damian.xBank.modules.user.user.domain.model;
 
 public enum UserRole {
     CUSTOMER, // App customers
-    EMPLOYEE, // Employees from the bank
     MANAGER, // Managers from the bank
     ADMIN // Admin of the application
 }
