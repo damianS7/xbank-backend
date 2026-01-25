202e8293b35ac11a8ed9a41cf186c4edbab22699
Damian <josepwnz@gmail.com>
2026-01-26 00:07:07 +0100

    refactor: remove spend

--- src/main/java/com/damian/xBank/modules/banking/card/infrastructure/web/controller/BankingCardOperationController.java
+++ src/main/java/com/damian/xBank/modules/banking/card/infrastructure/web/controller/BankingCardOperationController.java
@@ -1,8 +1,6 @@
 package com.damian.xBank.modules.banking.card.infrastructure.web.controller;
 
-import com.damian.xBank.modules.banking.card.application.dto.request.BankingCardSpendRequest;
 import com.damian.xBank.modules.banking.card.application.dto.request.BankingCardWithdrawRequest;
-import com.damian.xBank.modules.banking.card.application.usecase.BankingCardSpend;
 import com.damian.xBank.modules.banking.card.application.usecase.BankingCardWithdraw;
 import com.damian.xBank.modules.banking.transaction.application.dto.response.BankingTransactionDto;
 import com.damian.xBank.modules.banking.transaction.application.mapper.BankingTransactionDtoMapper;
@@ -17,14 +15,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 @RestController
 public class BankingCardOperationController {
-    private final BankingCardSpend bankingCardSpend;
     private final BankingCardWithdraw bankingCardWithdraw;
 
     public BankingCardOperationController(
-            BankingCardSpend bankingCardSpend,
             BankingCardWithdraw bankingCardWithdraw
     ) {
-        this.bankingCardSpend = bankingCardSpend;
         this.bankingCardWithdraw = bankingCardWithdraw;
     }
 
@@ -38,24 +33,6 @@ public class BankingCardOperationController {
     ) {
 
         BankingTransaction transaction = bankingCardWithdraw.execute(id, request);
-        BankingTransactionDto transactionDto = BankingTransactionDtoMapper
-                .toBankingTransactionDto(transaction);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(transactionDto);
-    }
-
-    // endpoint for logged customer to spend from card
-    @PostMapping("/banking/cards/{id}/spend")
-    public ResponseEntity<?> spend(
-            @PathVariable @NotNull @Positive
-            Long id,
-            @Validated @RequestBody
-            BankingCardSpendRequest request
-    ) {
-
-        BankingTransaction transaction = bankingCardSpend.execute(id, request);
         BankingTransactionDto transactionDto = BankingTransactionDtoMapper
                 .toBankingTransactionDto(transaction);
 
