3881eb7a217626594d730aebe7ec2bbeedf71927
Damian <josepwnz@gmail.com>
2026-01-31 23:59:44 +0100

    renamed and moved

--- src/main/java/com/damian/xBank/modules/payment/intent/application/usecase/GetPaymentIntent.java
+++ src/main/java/com/damian/xBank/modules/payment/intent/application/usecase/GetPaymentIntent.java
@@ -1,33 +1,27 @@
-package com.damian.xBank.modules.payment.checkout.application.usecase;
+package com.damian.xBank.modules.payment.intent.application.usecase;
 
 
+import com.damian.xBank.modules.payment.intent.domain.exception.PaymentIntentNotFoundException;
 import com.damian.xBank.modules.payment.intent.domain.model.PaymentIntent;
 import com.damian.xBank.modules.payment.intent.infrastructure.repository.PaymentIntentRepository;
 import org.springframework.stereotype.Service;
 
 /**
- * This class is used by the customer to get his pending payment.
+ * This usecase return a payment intent
  */
 @Service
-public class PaymentCheckoutGet {
+public class GetPaymentIntent {
     private final PaymentIntentRepository paymentIntentRepository;
 
-    public PaymentCheckoutGet(
+    public GetPaymentIntent(
             PaymentIntentRepository paymentIntentRepository
     ) {
         this.paymentIntentRepository = paymentIntentRepository;
     }
 
-    /**
-     *
-     * @param
-     * @return
-     */
     public PaymentIntent execute(Long id) {
-
         return paymentIntentRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Payment not found " + id)
+                () -> new PaymentIntentNotFoundException(id)
         );
-
     }
 }
