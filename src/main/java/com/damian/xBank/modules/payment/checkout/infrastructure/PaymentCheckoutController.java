00b6efc6c2f2ef81767826dc46ef8b55494a64f3
Damian <josepwnz@gmail.com>
2026-02-01 00:02:49 +0100

    added handlers.

--- src/main/java/com/damian/xBank/modules/payment/checkout/infrastructure/PaymentCheckoutController.java
+++ src/main/java/com/damian/xBank/modules/payment/checkout/infrastructure/PaymentCheckoutController.java
@@ -1,38 +1,51 @@
 package com.damian.xBank.modules.payment.checkout.infrastructure;
 
 import com.damian.xBank.modules.payment.checkout.application.dto.request.PaymentCheckoutSubmitRequest;
-import com.damian.xBank.modules.payment.checkout.application.usecase.PaymentCheckoutGet;
 import com.damian.xBank.modules.payment.checkout.application.usecase.PaymentCheckoutSubmit;
 import com.damian.xBank.modules.payment.checkout.domain.PaymentCheckoutForm;
 import com.damian.xBank.modules.payment.checkout.domain.excepcion.PaymentCheckoutException;
+import com.damian.xBank.modules.payment.intent.application.usecase.GetPaymentIntent;
 import com.damian.xBank.modules.payment.intent.domain.model.PaymentIntent;
 import com.damian.xBank.modules.payment.intent.domain.model.PaymentIntentStatus;
+import com.damian.xBank.shared.exception.ErrorCodes;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Validated
 @Controller
 public class PaymentCheckoutController {
     private static final Logger log = LoggerFactory.getLogger(PaymentCheckoutController.class);
-    private final PaymentCheckoutGet paymentCheckoutGet;
+    private final GetPaymentIntent getPaymentIntent;
     private final PaymentCheckoutSubmit paymentCheckoutSubmit;
+    private final MessageSource messageSource;
 
     public PaymentCheckoutController(
-            PaymentCheckoutGet paymentCheckoutGet,
-            PaymentCheckoutSubmit paymentCheckoutSubmit
+            GetPaymentIntent getPaymentIntent,
+            PaymentCheckoutSubmit paymentCheckoutSubmit,
+            MessageSource messageSource
     ) {
-        this.paymentCheckoutGet = paymentCheckoutGet;
+        this.getPaymentIntent = getPaymentIntent;
         this.paymentCheckoutSubmit = paymentCheckoutSubmit;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/payments/{id}/checkout")
@@ -41,14 +54,13 @@ public class PaymentCheckoutController {
             Model model
     ) {
         // Get payment
-        PaymentIntent paymentIntent = paymentCheckoutGet.execute(id);
+        PaymentIntent paymentIntent = getPaymentIntent.execute(id);
         if (paymentIntent.getStatus() != PaymentIntentStatus.PENDING) {
             return "redirect:/payments/" + id + "/status";
         }
 
         // form
         PaymentCheckoutForm form = new PaymentCheckoutForm();
-        form.setPaymentId(paymentIntent.getId());
 
         // read only fields
         model.addAttribute("paymentId", paymentIntent.getId());
@@ -68,7 +80,7 @@ public class PaymentCheckoutController {
             Model model
     ) {
         // Get payment
-        PaymentIntent paymentIntent = paymentCheckoutGet.execute(id);
+        PaymentIntent paymentIntent = getPaymentIntent.execute(id);
 
         // read only fields
         model.addAttribute("paymentId", paymentIntent.getId());
@@ -83,33 +95,32 @@ public class PaymentCheckoutController {
 
     @PostMapping("/payments/checkout")
     public String paymentCheckoutSubmit(
-            PaymentCheckoutForm form, // TODO Validate?
+            @Valid PaymentCheckoutForm form,
             Model model
     ) {
-        log.debug("Processing payment with id: {}", form.getPaymentId());
-        if (form.getPaymentId() == null) {
+        log.debug("Processing payment: {}", form.paymentId());
+        if (form.paymentId() == null) {
             model.addAttribute("error", "Invalid payment id");
             return "layout/error";
         }
 
-        try {
+        try { // TODO Remove try?
             paymentCheckoutSubmit.execute(
                     new PaymentCheckoutSubmitRequest(
-                            form.getPaymentId(),
-                            form.getCardHolder(),
-                            form.getCardNumber(),
-                            form.getCvv(),
-                            form.getCardPin(),
-                            form.getExpiryMonth(),
-                            form.getExpiryYear(),
-                            form.getMerchantCallbackUrl()
+                            form.paymentId(),
+                            form.cardHolder(),
+                            form.cardNumber(),
+                            form.cvv(),
+                            form.cardPin(),
+                            form.expiryMonth(),
+                            form.expiryYear()
                     )
             );
         } catch (Exception exception) {
-            throw new PaymentCheckoutException(form.getPaymentId(), exception.getMessage());
+            throw new PaymentCheckoutException(form.paymentId(), exception.getMessage());
         }
 
-        return "redirect:/payments/" + form.getPaymentId() + "/status";
+        return "redirect:/payments/" + form.paymentId() + "/status";
     }
 
     @ExceptionHandler(PaymentCheckoutException.class)
@@ -128,6 +139,50 @@ public class PaymentCheckoutController {
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public String handleException(HttpRequestMethodNotSupportedException ex, Model model) {
         model.addAttribute("error", "Internal server error");
+        return "layout/error";
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class) // 404
+    public String handleException(
+            ConstraintViolationException ex,
+            Model model
+    ) {
+        log.warn("Validation error found: {}", ex.getMessage(), ex);
+        model.addAttribute("error", ErrorCodes.VALIDATION_FAILED);
+        model.addAttribute("errorCode", ErrorCodes.VALIDATION_FAILED);
+        return "layout/error";
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class) // 404
+    public String handleException(
+            HandlerMethodValidationException ex,
+            Model model
+    ) {
+        log.warn("Validation error found: {}", ex.getMessage(), ex);
+        model.addAttribute("error", ErrorCodes.VALIDATION_FAILED);
+        model.addAttribute("errorCode", ErrorCodes.VALIDATION_FAILED);
+        return "layout/error";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // 400
+    public String handleException(
+            MethodArgumentNotValidException ex,
+            Model model
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        log.warn("Validation failed: {} errors -> {}", errors.size(), errors);
+        model.addAttribute(
+                "error", messageSource.getMessage(
+                        ErrorCodes.VALIDATION_FAILED,
+                        null,
+                        LocaleContextHolder.getLocale()
+                )
+        );
+        model.addAttribute("errors", errors);
         return "layout/error";
     }
 }
